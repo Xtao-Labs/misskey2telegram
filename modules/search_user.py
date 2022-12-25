12f5f8391a164b128b448a20dedd6ca1076c004f
xtaodada <xtao@xtaolink.cn>
2022-12-25 22:42:07 +0800

    feat: follow user

--- modules/search_user.py
+++ modules/search_user.py
@@ -1,9 +1,11 @@
+from mipac.errors import InternalErrorError, AlreadyFollowingError, FolloweeIsYourselfError
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, CallbackQuery
 
 from glover import admin
 
 from defs.search_user import search_user, gen_text, gen_button
+from misskey_init import misskey_bot
 
 
 # @xxx
@@ -30,3 +32,30 @@ async def search_user_command(_: Client, message: Message):
             await message.reply(text, reply_markup=button, quote=True, disable_web_page_preview=True)
     except Exception as e:
         return await message.reply(f"搜索用户失败：{e}", quote=True)
+
+
+# follow:xxx
+@Client.on_callback_query(filters.regex(r"^follow:(.*)$"))
+async def follow_user_callback(_: Client, callback_query: CallbackQuery):
+    """
+        关注/取消关注用户
+    """
+    user_id = callback_query.matches[0].group(1)
+    try:
+        await misskey_bot.core.api.follow.action.add(user_id)
+        await callback_query.answer("关注成功", show_alert=True)
+    except InternalErrorError:
+        await callback_query.answer("关注申请未批准，请等待对方同意", show_alert=True)
+    except AlreadyFollowingError:
+        try:
+            await misskey_bot.core.api.follow.action.remove(user_id)
+            await callback_query.answer("取消关注成功", show_alert=True)
+        except Exception as e:
+            await callback_query.answer("取消关注失败", show_alert=True)
+            await callback_query.message.reply(f"取消关注失败：{e}", quote=True)
+        return
+    except FolloweeIsYourselfError:
+        await callback_query.answer("不能关注自己", show_alert=True)
+    except Exception as e:
+        await callback_query.answer("关注失败", show_alert=True)
+        await callback_query.message.reply(f"关注失败：{e}", quote=True)
