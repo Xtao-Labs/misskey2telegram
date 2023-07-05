59be2569f4790f10e05c42205dfd2273229d67be
xtaodada <xtao@xtaolink.cn>
2023-07-05 20:10:41 +0800

    feat: keep daily online

--- modules/announcement.py
+++ modules/announcement.py
@@ -7,8 +7,6 @@ from misskey_init import misskey_bot_map
 @scheduler.scheduled_job("interval", minutes=15, id="check_announcement")
 async def announcement():
     for bot in misskey_bot_map.values():
-        if not bot.tg_user:
-            continue
         data = await get_unread_announcements(bot)
         for an in data:
             try:
--- modules/timeline.py
+++ modules/timeline.py
@@ -1,4 +1,14 @@
+import contextlib
+
 from init import bot
-from misskey_init import init_misskey_bot
+from misskey_init import init_misskey_bot, misskey_bot_map
+from models.services.scheduler import scheduler
 
 bot.loop.create_task(init_misskey_bot())
+
+
+@scheduler.scheduled_job("cron", hour=1, id="daily_status")
+async def daily_status():
+    for m_bot in misskey_bot_map.values():
+        with contextlib.suppress(Exception):
+            await m_bot.core.api.get_me()
